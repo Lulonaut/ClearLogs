697d14d15e8e591dfb9b1d2d891c4ecfeed9b71a
Lulonaut <lulonaut@tutanota.de>
2022-07-09 10:52:39 +0200

    also delete crash reports

--- src/main/java/de/lulonaut/clearlogs/ClearLogs.java
+++ src/main/java/de/lulonaut/clearlogs/ClearLogs.java
@@ -5,6 +5,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @Mod(modid = "clearlogs", name = "ClearLogs", version = "1.0")
 public class ClearLogs {
@@ -17,8 +21,23 @@ public class ClearLogs {
             File[] logFiles = logsFolder.listFiles();
             if (logFiles != null) {
                 for (File file : logFiles) {
+                    //Delete all but the latest log
                     if (file.getName().endsWith("log.gz")) {
                         file.delete();
+                    }
+                }
+            }
+        }
+        File crashReportsFolder = new File(Minecraft.getMinecraft().mcDataDir, "crash-reports");
+        if (crashReportsFolder.exists()) {
+            File[] reports = crashReportsFolder.listFiles();
+            if (reports != null) {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+                String formattedDate = dateFormat.format(new Date());
+                for (File report : reports) {
+                    //Delete all reports that are not from today
+                    if (!report.getName().startsWith("crash-" + formattedDate)) {
+                        report.delete();
                     }
                 }
             }
